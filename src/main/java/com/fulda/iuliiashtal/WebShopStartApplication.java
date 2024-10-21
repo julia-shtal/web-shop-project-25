d45175cd209b4c8b543132afd208f01ac26228c9
fd0001560 <fd0001560@fhfddvz1.rz.hs-fulda.de>
2024-10-21 14:50:18 +0200

    0-4 Create application

---
+++ src/main/java/com/fulda/iuliiashtal/WebShopStartApplication.java
@@ -0,0 +1,13 @@
+package com.fulda.iuliiashtal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuliiashtalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fulda/iuliiashtal/IuliiashtalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulda.iuliiashtal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IuliiashtalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
